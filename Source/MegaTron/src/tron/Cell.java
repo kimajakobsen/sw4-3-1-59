0cccde2496f3ee8b1671ef3a415e97e2a4723bec
Rasmus Prentow <rprentow@gmail.com>
2011-03-11 10:20:04 +0100

    Tron

--- Source/MegaTron/src/tron/Cell.java
+++ Source/MegaTron/src/tron/Cell.java
@@ -4,6 +4,9 @@ import java.awt.Color;
 
 public class Cell implements Drawable {
 
+	public static int x;
+	public static int y;
+	
 	@Override
 	public void draw() {
 		// TODO Auto-generated method stub
