6c7f52b6014e64a23213aba377098a991b55f897
Rasmus Prentow <rprentow@gmail.com>
2011-03-11 09:47:09 +0100

    Tron

--- Source/MegaTron/src/tron/Master.java
+++ Source/MegaTron/src/tron/Master.java
@@ -1,9 +1,13 @@
 package tron;
 
+import java.awt.Color;
+
 public class Master {
 
+	public static Grid grid; 
+	
 	public static void main(String[] args){
-		
+		grid = new Grid();
 		System.out.println("dhhdd");
 	}
 	
@@ -12,4 +16,9 @@ public class Master {
 		
 	}
 	
+	
+	void setGridColor(int posX, int posY, Color c){
+		grid.setGridColor(posX,posY,c);
+		
+	}
 }
