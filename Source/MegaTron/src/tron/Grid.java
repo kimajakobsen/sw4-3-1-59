937479cbb5b32358058fc3c36624b912a258125b
Rasmus Prentow <rprentow@gmail.com>
2011-03-11 09:38:26 +0100

    Tron

--- Source/MegaTron/src/tron/Grid.java
+++ Source/MegaTron/src/tron/Grid.java
@@ -1,11 +1,16 @@
 package tron;
 
+import java.util.ArrayList;
+
 public class Grid implements Drawable {
 
+	public ArrayList<Cell> cells;
+	
 	@Override
 	public void draw() {
 		// TODO Auto-generated method stub
 		
 	}
+	
 
 }
