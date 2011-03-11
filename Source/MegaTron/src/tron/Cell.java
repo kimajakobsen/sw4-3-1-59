baece9740318d53ef2b5b0b625615bb8b32e9b18
Rasmus Prentow <rprentow@gmail.com>
2011-03-11 10:40:05 +0100

    Tron

--- Source/MegaTron/src/tron/Cell.java
+++ Source/MegaTron/src/tron/Cell.java
@@ -4,8 +4,8 @@ import java.awt.Color;
 
 public class Cell implements Drawable {
 
-	public static int height;
-	public static int width;
+	public static int height = 5;
+	public static int width = 5;
 	
 	@Override
 	public void draw() {
