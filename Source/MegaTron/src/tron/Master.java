6a2cbc4aa9f2d7cc99ed9f298f4667661fe67a30
abondoa <abondoa@gmail.com>
2011-03-11 09:48:58 +0100

    Merge

---
+++ Source/MegaTron/src/tron/Master.java
@@ -0,0 +1,15 @@
+package tron;
+
+public class Master {
+
+	public static void main(String[] args){
+		
+		System.out.println("dhhdd");
+	}
+	
+	void RedTron(){
+		
+		
+	}
+	
+}
--- codeExamples/RecursivedescentParser/src/parser/Compiler.java
+++ codeExamples/RecursivedescentParser/src/parser/Compiler.java
@@ -11,7 +11,7 @@ public class Compiler {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		StringBuffer b = new StringBuffer("a =     5    +  2  ; sdfsd=4                  *(a + 4) + (7); \000");
+		StringBuffer b = new StringBuffer("k = (3433303003 + 5555) * 10; a =     5    +  2  ; sdfsd=4                 *(a + 4) + (7); \000");
 		//	Scanner scanner = new Scanner(b);
 			Parser parser = new Parser(b);
 			Program ast = (Program) parser.parse();
