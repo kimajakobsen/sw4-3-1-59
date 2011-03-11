d9905a4a60965dca15b3cca7e8c6c0bd7edc9e95
Rasmus Prentow <rprentow@gmail.com>
2011-03-11 09:54:51 +0100

    Tron

--- Source/MegaTron/src/tron/Grid.java
+++ Source/MegaTron/src/tron/Grid.java
@@ -8,15 +8,16 @@ import javax.swing.JPanel;
 public class Grid  extends JPanel implements Drawable {
 
 	public Cell[][] cells;
-
+	
 	@Override
 	public void draw() {
+		
 		// TODO Auto-generated method stub
-		cells = new Cell[30][30]();
+		cells = new Cell[30][30];
 	}
 
 	public void setGridColor(int posX, int posY, Color c){
-
+		
 
 
 	}
